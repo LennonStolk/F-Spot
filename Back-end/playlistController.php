bed71c431583e02cdbbc02b6125d5ece04ed82d8
LennonStolk <70744566+LennonStolk@users.noreply.github.com>
2022-03-17 10:13:26 +0100

    createPlaylist toegevoegd aan playlistController

--- Back-end/playlistController.php
+++ Back-end/playlistController.php
@@ -17,6 +17,60 @@ else {
     echo(json_encode('noMethodFound'));;
 }
 
+if ($method = "createPlaylist") {
+    $userName = isset($_POST["userName"]) ? $_POST["userName"] : null;
+    $password = isset($_POST["password"]) ? $_POST["password"] : null;
+    $playlistName = isset($_POST["playlistName"]) ? $_POST["playlistName"] : null;
+
+    if (authenticate($db, $userName, $password) == false) {
+        echo(json_encode('authenticationFailed'));
+        die();
+    }
+
+    createPlaylist($db, $userName, $playlistName);
+}
+
+function createPlaylist($db, $userName, $playlistName) {
+    // Check conditions
+    if (strlen($playlistName) > 15) {
+        echo(json_encode('playlistNameTooLong'));
+        die();
+    }
+    elseif ($playlistName == "") {
+        echo(json_encode('emptyFields'));
+        die();
+    }
+    elseif (checkLegalCharacters($userName, "abcdefghijklmnopqrstuvwxyz1234567890_-") == false) {
+        echo(json_encode('illegalCharacters'));
+        die();
+    }
+
+    // Get id from user
+    $userId = getIdFromUserName($db, $userName);
+
+    // Insert playlist into database
+    $sql = 'INSERT INTO fspot_playlists (name, userId) VALUES (:playlistName, :userId);';
+    $vars = [
+        "playlistName" => $playlistName,
+        "userId" => $userId,
+    ];
+    $stmt = $db->prepare($sql);
+    $stmt->execute($vars);
+
+    echo(json_encode('successfullyMadePlaylist'));
+    die();
+}
+
+function getIdFromUserName($db, $userName) {
+    $sql = 'SELECT id FROM fspot_users WHERE username = :userName';
+    $vars = ["userName" => $userName];
+    $stmt = $db->prepare($sql);
+    $stmt->execute($vars);
+    $result = $stmt->fetch()[0];
+
+    return $result;
+}
+
 function getPasswordHash($db, $userName) {
     $sql = 'SELECT password FROM fspot_users WHERE username = :userName';
     $vars = ["userName" => $userName];
