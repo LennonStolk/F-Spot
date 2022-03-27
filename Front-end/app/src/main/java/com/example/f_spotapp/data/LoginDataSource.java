83e620c84e2d713fc74096e11f41e13f115e9518
LennonStolk <70744566+LennonStolk@users.noreply.github.com>
2022-03-27 01:16:15 +0100

    Kleine bug met inlog notificatie gefixt

--- Front-end/app/src/main/java/com/example/f_spotapp/data/LoginDataSource.java
+++ Front-end/app/src/main/java/com/example/f_spotapp/data/LoginDataSource.java
@@ -15,8 +15,8 @@ public class LoginDataSource {
             // TODO: handle loggedInUser authentication
             LoggedInUser fakeUser =
                     new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Michael");
+                            username,
+                            password);
             return new Result.Success<>(fakeUser);
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
